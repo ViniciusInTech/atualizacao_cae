235d70dc37710c42ba2662b6b991f6e044f7ef4d
viniciusintech <vini989073599@gmail.com>
2024-10-11 23:26:20 -0300

    feat: teste de para verificar se consegue atualizar de fato

--- cae/src/main/java/com/github/viniciusintech/Main.java
+++ cae/src/main/java/com/github/viniciusintech/Main.java
@@ -4,6 +4,13 @@ import static com.github.viniciusintech.GitUpdater.updateProject;
 
 public class Main {
     public static void main(String[] args) {
+        // novas linhas para testar se de fato ele atualiza o código de modo automatico
+
+
+
+
+        //comentarios para teste de código
+        String nome = "teste";
         String projectPath = "C:\\Users\\desktop\\Documents\\atualizacao_cae";
         updateProject(projectPath);
     }
